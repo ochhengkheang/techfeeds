01e1a1e9c4c6d8d3d7f4148fba53422fa12cb12e
ochhengkheang <ochhengkheang@gmail.com>
2023-02-22 11:59:51 +0700

    write status, app expection

--- lib/res/app_url.dart
+++ lib/res/app_url.dart
@@ -1,1 +1,7 @@
-
+class AppUrl {
+  static String baseUrl = "https://cms.istad.co/api";
+  static String getArticle = "$baseUrl/articles?populate=thumbnail";
+  static String getCategory = "$baseUrl/categories";
+  static String uploadImage = "$baseUrl/upload";
+  static String postArticle = "$baseUrl/articles";
+}
--- lib/views/home/homescreen.dart
+++ lib/views/home/homescreen.dart
@@ -9,58 +9,101 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  var darkblue = Color.fromRGBO(18, 17, 56, 1);
-  var lightgreen = Color.fromRGBO(79, 192, 159, 1);
+  var darkBlue = Color.fromRGBO(18, 17, 56, 1);
+  var lightGreen = Color.fromRGBO(79, 192, 159, 1);
+  var fontStyleCategory = GoogleFonts.poppins(
+      fontWeight: FontWeight.w500, color: Colors.white, fontSize: 16);
+  var onCategory = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: [
-          SizedBox(
-            width: 85,
-            child: TextButton(
-              onPressed: () {},
-              child: Text("POST",
-                  style: GoogleFonts.poppins(
-                      textStyle: Theme.of(context).textTheme.bodyLarge,
-                      fontWeight: FontWeight.bold,
-                      color: lightgreen,
-                      fontSize: 18)),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(65),
+        child: AppBar(
+          toolbarHeight: 100.0,
+          elevation: 0.0,
+          automaticallyImplyLeading: false,
+          flexibleSpace: Container(),
+          actions: [
+            SizedBox(
+              width: 85,
+              child: TextButton(
+                  onPressed: () {},
+                  child: Text("POST",
+                      style: GoogleFonts.poppins(
+                          fontWeight: FontWeight.bold,
+                          color: lightGreen,
+                          fontSize: 22))),
+            )
+          ],
+          backgroundColor: Colors.white,
+          title: Text("HOME",
+              style: GoogleFonts.poppins(
+                  fontWeight: FontWeight.bold, color: darkBlue, fontSize: 18)),
+          leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: darkBlue,
+              size: 28,
             ),
-          )
-        ],
-        backgroundColor: Colors.white,
-        title: Text("HOME",
-            style: GoogleFonts.poppins(
-                textStyle: Theme.of(context).textTheme.bodyLarge,
-                fontWeight: FontWeight.bold,
-                color: darkblue,
-                fontSize: 18)),
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: darkblue,
-            size: 28,
+            onPressed: () {},
           ),
-          onPressed: () {},
+          leadingWidth: 33,
         ),
-        leadingWidth: 33,
       ),
-      body: Container(),
+      body: Container(
+        margin: EdgeInsets.fromLTRB(10, 0, 10, 10),
+        child: Column(
+          children: [
+            Container(
+              height: 55,
+              width: double.infinity,
+              color: darkBlue,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  textButton(Icons.home, 1, "ALL"),
+                  textButton(Icons.web, 2, "Web Developer"),
+                  textButton(Icons.code, 3, "Programming"),
+                  textButton(Icons.code, 4, "AI / Big Data"),
+                  textButton(Icons.analytics_outlined, 5, "Backend"),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
       bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: darkblue,
+        backgroundColor: darkBlue,
         currentIndex: 1,
         showSelectedLabels: false,
         showUnselectedLabels: false,
         items: [
           BottomNavigationBarItem(
-              label: "Home", icon: Icon(Icons.home, color: lightgreen)),
+              label: "Home", icon: Icon(Icons.home, color: lightGreen)),
           BottomNavigationBarItem(
               label: "Manage",
-              icon: Icon(Icons.app_registration_rounded, color: lightgreen))
+              icon: Icon(Icons.app_registration_rounded, color: lightGreen))
         ],
       ),
+    );
+  }
+
+  TextButton textButton(var icon, var optionNumber, var title) {
+    return TextButton.icon(
+      icon: Icon(icon,
+          color: onCategory == optionNumber ? lightGreen : Colors.white),
+      label: Text(title,
+          style: GoogleFonts.poppins(
+              fontWeight: FontWeight.w500,
+              color: onCategory == optionNumber ? lightGreen : Colors.white,
+              fontSize: 16)),
+      onPressed: () {
+        setState(() {
+          onCategory = optionNumber;
+        });
+      },
     );
   }
 }
